328bccc5531233fe6278ea2c5b11ba9f8a4884e2
Eden AI <67952486+edenai@users.noreply.github.com>
2021-07-30 18:11:25 +0200

    Create edenai-explicit-content-detection.js

---
+++ api_requests/edenai-explicit-content-detection.js
@@ -0,0 +1,18 @@
+var axios = require('axios');
+var FormData = require('form-data');
+var fs = require('fs');
+var data = new FormData();
+data.append('files', fs.createReadStream('PATH TO YOUR FILE'));
+data.append('providers', '['PROVIDER(S)']');
+
+var config = {
+  method: 'post',
+  url: 'https://api.edenai.run/v1/pretrained/vision/explicit_content_detection',
+  headers: { 
+    'Authorization': 'Bearer your_api_key', 
+    ...data.getHeaders()
+  },
+  data : data
+};
+
+axios(config)
