561a11062064363d7b0da21b1b2a45aeabc2d1bd
Eden AI <67952486+edenai@users.noreply.github.com>
2021-07-30 18:15:07 +0200

    Create edenai-keyword-extraction.js

---
+++ api_requests/edenai-keyword-extraction.js
@@ -0,0 +1,18 @@
+var axios = require('axios');
+var FormData = require('form-data');
+var data = new FormData();
+data.append('text', ''YOUR TEXT'');
+data.append('providers', '['PROVIDER(S)']');
+data.append('language', 'LANGUAGE');
+
+var config = {
+  method: 'post',
+  url: 'https://api.edenai.run/v1/pretrained/text/keyword_extraction',
+  headers: { 
+    'Authorization': 'Bearer your_api_key', 
+    ...data.getHeaders()
+  },
+  data : data
+};
+
+axios(config)
